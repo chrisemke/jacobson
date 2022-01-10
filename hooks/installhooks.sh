56489e47b8b886982e6ad33a5356d9fd216e573a
chrisemke <chrisemke@protonmail.com>
2022-01-10 20:53:29 -0300

    chore(hooks): Regex body
    Fixed footer when there's no body + code smell
    
    Quality: commit

--- hooks/installhooks.sh
+++ hooks/installhooks.sh
@@ -10,20 +10,20 @@ CURDIR=$(basename "$PWD")
 
 echo '\033[1;34m➡ Installing all the githooks...\033[0m'
 
-if [[ $CURDIR != "hooks" ]]; then echo '\033[1;31m✘ You need to execute this command from the hooks/ directory\033[0m'
-    exit 1
+if [[ $CURDIR != "hooks" ]]; then
+  echo '\033[1;31m✘ You need to execute this command from the hooks/ directory\033[0m'
+  exit 1
 fi
 
 # Create symlink for each githook into $GITDIR
-for file in $(ls -A)
-do
-  if [[ $file == "installhooks.sh" ]];
-    then continue
-  elif [[ -x $file ]];
-    then ln -svf "$PWD/$file" "$GITDIR"
+for file in $(ls -A); do
+  if [[ $file == "installhooks.sh" ]]; then
+    continue
+  elif [[ -x $file ]]; then
+    ln -svf "$PWD/$file" "$GITDIR"
   else
-      echo '\033[1;34m➡ Skipping' "$file" 'because you cannot execute it\033[0m'
-    fi
+    echo '\033[1;34m➡ Skipping' "$file" 'because you cannot execute it\033[0m'
+  fi
 done
 echo '\033[1;32m✔︎ Githooks successfully installed...\033[0m'
 
