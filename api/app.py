f75116c5206fb425698b4e6aeb099e0a309830b7
Christian G. Semke <chrisemke@protonmail.com>
2024-02-05 23:40:51 -0300

    chore: fix git hooks, more black removes, update more deps

--- api/app.py
+++ api/app.py
@@ -16,27 +16,9 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from os import getenv
-from urllib.parse import quote_plus
+from fastapi import FastAPI
 
-from dotenv import load_dotenv
-from sqlmodel import SQLModel, create_engine
+from api.schema import graphql_app
 
-load_dotenv()
-
-
-DATABASE_USER = getenv('DATABASE_USER')
-DATABASE_PASSWORD = quote_plus(getenv('DATABASE_PASSWORD'))
-DATABASE_HOST = getenv('DATABASE_HOST')
-DATABASE_PORT = getenv('DATABASE_PORT')
-DATABASE_NAME = getenv('DATABASE_NAME')
-
-args = (
-    f'mariadb+mariadbconnector://'
-    f'{DATABASE_USER}:{DATABASE_PASSWORD}@'
-    f'{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
-)
-
-engine = create_engine(args)
-
-SQLModel.metadata.create_all(engine)
+app = FastAPI()
+app.include_router(graphql_app, prefix='/graphql')
--- jacobson/entities/address.py
+++ jacobson/entities/address.py
@@ -1,29 +1,29 @@
 """
- Jacobson is a self hosted zipcode API
- Copyright (C) 2023  Christian G. Semke
+Jacobson is a self hosted zipcode API
+Copyright (C) 2023-2024  Christian G. Semke.
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from sqlmodel import Field, Relationship, SQLModel, UniqueConstraint
 
 
 class Address(SQLModel, table=True):
-    __table_args__ = (UniqueConstraint("zipcode"),)
+    __table_args__ = (UniqueConstraint('zipcode'),)
     zipcode: int
-    city: int = Field(default=None, foreign_key="city.ibge")
-    state: int = Field(default=None, foreign_key="state.id")
+    city: int = Field(default=None, foreign_key='city.ibge')
+    state: int = Field(default=None, foreign_key='state.id')
     district: str
     complement: str
     neighborhood: str
@@ -31,15 +31,15 @@ class Address(SQLModel, table=True):
     longitude: float
     altitude: float
 
-    States: list["State"] = Relationship(back_populates="address")
-    Cities: list["City"] = Relationship(back_populates="address")
+    States: list['State'] = Relationship(back_populates='address')
+    Cities: list['City'] = Relationship(back_populates='address')
 
 
 class State(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     state: str
 
-    adresses: list["Address"] = Relationship(back_populates="state")
+    adresses: list['Address'] = Relationship(back_populates='state')
 
 
 class City(SQLModel, table=True):
@@ -47,4 +47,4 @@ class City(SQLModel, table=True):
     name: str
     ddd: int | None
 
-    adresses: list["Address"] = Relationship(back_populates="city")
+    adresses: list['Address'] = Relationship(back_populates='city')
